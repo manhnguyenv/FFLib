4ef4848b4e43060d47eed942a458902ed6acb24b
Phil Bird <pbird@fast-forward-it.com>
2012-08-08 12:25:10 -0600

    Ad V2 project files
    Signed-off-by: Phil Bird <pbird@fast-forward-it.com>

---
+++ FFLib/Extentions/String.cs
@@ -0,0 +1,28 @@
+﻿/*******************************************************
+ * Project: FFLib V1.0
+ * Title: String.cs
+ * Author: Phillip Bird of Fast Forward,LLC
+ * Copyright © 2012 Fast Forward, LLC. 
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * Use of any component of FFLib requires acceptance and adhearance 
+ * to the terms of either the MIT License or the GNU General Public License (GPL) Version 2 exclusively.
+ * Notification of license selection is not required and will be infered based on applicability.
+ * Contributions to FFLib requires a contributor grant on file with Fast Forward, LLC.
+********************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FFLib.Extensions
+{
+    public static class StringExtensions_General
+    {
+        public static string SubstringEx(this string self, int StartIndex, int Length)
+        {
+            if (Length > self.Length - StartIndex) Length = self.Length - StartIndex;
+            return self.Substring(StartIndex, Length);
+        }
+    }
+}
