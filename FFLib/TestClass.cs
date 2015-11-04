81e1f4b7882921d61ab527efe10e41b36907ab0f
Phil Bird <pbird@fast-forward-it.com>
2015-11-04 09:36:18 -0700

    Add Test Class
    Signed-off-by: Phil Bird <pbird@fast-forward-it.com>

---
+++ FFLib/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FFLib
+{
+    //testing Jenkins
+    public class TestClass
+    {
+        public string Value { get; set; }
+    }
+}
