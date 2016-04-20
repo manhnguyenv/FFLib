27733c9ed1d0b1793cde3e2a4d2d533baac13abc
Phil Bird <pbird@fast-forward-it.com>
2016-04-20 13:45:48 -0600

    Add extension method AddOrReplace
    Signed-off-by: Phil Bird <pbird@fast-forward-it.com>

--- FFLib/Extensions/Dictionary.cs
+++ FFLib/Extensions/Dictionary.cs
@@ -13,5 +13,11 @@ namespace FFLib.Extensions
             if (!self.ContainsKey(key)) return default(T) ;
             return self[key];
         }
+
+        public static void AddOrReplace<K,T>(this Dictionary<K, T> self, K key, T value)
+        {
+            if (self.ContainsKey(key)) self[key] = value;
+            else self.Add(key, value);
+        }
     }
 }
