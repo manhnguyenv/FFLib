558a91407b12e662a2982f8f47ecf55ee69378fa
Phil Bird <pbird@fast-forward-it.com>
2017-08-29 15:42:44 -0600

    Convert Mock framework from RhinoMocks to NSubstitute. Update Nunit to V3.x
    Signed-off-by: Phil Bird <pbird@fast-forward-it.com>

--- FFLibUnitTests/Data/DBTableTests.cs
+++ FFLibUnitTests/Data/DBTableTests.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
-using Rhino;
-using Rhino.Mocks;
+using NSubstitute;
 
 namespace FFLibUnitTests.Data
 {
@@ -14,58 +13,14 @@ namespace FFLibUnitTests.Data
         [Test]
         public void Load()
         {
-            MockRepository mocks = new MockRepository(); 
-            FFLib.Data.DBProviders.IDBProvider dbprovider = mocks.Stub<FFLib.Data.DBProviders.IDBProvider>();
-            FFLib.Data.IDBConnection Conn = mocks.Stub<FFLib.Data.IDBConnection>();
+            //Initialize
+            FFLib.Data.DBProviders.IDBProvider dbprovider = Substitute.For<FFLib.Data.DBProviders.IDBProvider>();
+            FFLib.Data.IDBConnection Conn = Substitute.For<FFLib.Data.IDBConnection>();
             FFLib.Data.DBTable<TestDTO> dtoTable;
             TestDTO expected = new TestDTO();
             expected.ID = 1;
 
-
-            System.Data.DataTable dr = new System.Data.DataTable();
-            dr.Columns.Add("ID", typeof(int));
-            dr.Columns.Add("Name",typeof(String));
-            dr.Columns.Add("Status", typeof(string));
-            dr.Columns.Add("CreateDate", typeof(DateTime));
-            System.Data.DataRow drRow = dr.NewRow();
-            drRow["ID"] = 1;
-            drRow["Name"] = "test";
-            drRow["Status"] = "open";
-            drRow["CreateDate"] = DateTime.Now;
-            dr.Rows.Add(drRow);
-
-            System.Data.IDataReader idr = dr.CreateDataReader();
-
-            using (mocks.Record())
-            {
-                dbprovider.Stub(x => x.ExecuteReader(null,null,null)).IgnoreArguments().Repeat.Any().Return(idr);
-                Conn.Open();
-                LastCall.Repeat.Any();
-                Conn.Stub(x => x.InTrx).Repeat.Any().Return(false);
-                Conn.Stub(x => x.dbProvider).Return((FFLib.Data.DBProviders.IDBProvider)dbprovider).Repeat.Any();
-                Conn.Stub(x => x.State).Repeat.Any().Return(System.Data.ConnectionState.Closed);
-            }
-
-            using (mocks.Playback())
-            {
-                dtoTable = new FFLib.Data.DBTable<TestDTO>(Conn);
-                TestDTO result = dtoTable.Load(1);
-                Assert.AreEqual(expected.ID, result.ID);
-            }
-
-        }
-
-        [Test]
-        public void LoadWithFieldMap()
-        {
-            MockRepository mocks = new MockRepository();
-            FFLib.Data.DBProviders.IDBProvider dbprovider = mocks.Stub<FFLib.Data.DBProviders.IDBProvider>();
-            FFLib.Data.IDBConnection Conn = mocks.Stub<FFLib.Data.IDBConnection>();
-            FFLib.Data.DBTable<TestDTO2> dtoTable;
-            TestDTO2 expected = new TestDTO2();
-            expected.XID = 1;
-
-
+            //Setup
             System.Data.DataTable dr = new System.Data.DataTable();
             dr.Columns.Add("ID", typeof(int));
             dr.Columns.Add("Name", typeof(String));
@@ -80,31 +35,64 @@ namespace FFLibUnitTests.Data
 
             System.Data.IDataReader idr = dr.CreateDataReader();
 
-            using (mocks.Record())
-            {
-                dbprovider.Stub(x => x.ExecuteReader(null, null, null)).IgnoreArguments().Repeat.Any().Return(idr);
-                Conn.Open();
-                LastCall.Repeat.Any();
-                Conn.Stub(x => x.InTrx).Repeat.Any().Return(false);
-                Conn.Stub(x => x.dbProvider).Return((FFLib.Data.DBProviders.IDBProvider)dbprovider).Repeat.Any();
-                Conn.Stub(x => x.State).Repeat.Any().Return(System.Data.ConnectionState.Closed);
-            }
+            Conn.InTrx.Returns<bool>(false);
+            Conn.dbProvider.Returns<FFLib.Data.DBProviders.IDBProvider>(dbprovider);
+            Conn.State.Returns<System.Data.ConnectionState>(System.Data.ConnectionState.Closed);
 
-            using (mocks.Playback())
-            {
-                dtoTable = new FFLib.Data.DBTable<TestDTO2>(Conn);
-                TestDTO2 result = dtoTable.Load(1);
-                Assert.AreEqual(expected.XID, result.XID);
-            }
+            dbprovider.ExecuteReader(null, null, null).ReturnsForAnyArgs(idr);
 
+            //Execute
+            dtoTable = new FFLib.Data.DBTable<TestDTO>(Conn);
+            TestDTO result = dtoTable.Load(1);
+            Assert.AreEqual(expected.ID, result.ID);
+
+
+        }
+
+        [Test]
+        public void LoadWithFieldMap()
+        {
+            //Initialize
+            FFLib.Data.DBProviders.IDBProvider dbprovider = Substitute.For<FFLib.Data.DBProviders.IDBProvider>();
+            FFLib.Data.IDBConnection Conn = Substitute.For<FFLib.Data.IDBConnection>();
+            FFLib.Data.DBTable<TestDTO2> dtoTable;
+            TestDTO2 expected = new TestDTO2();
+            expected.XID = 1;
+
+            //Setup
+            System.Data.DataTable dr = new System.Data.DataTable();
+            dr.Columns.Add("ID", typeof(int));
+            dr.Columns.Add("Name", typeof(String));
+            dr.Columns.Add("Status", typeof(string));
+            dr.Columns.Add("CreateDate", typeof(DateTime));
+            System.Data.DataRow drRow = dr.NewRow();
+            drRow["ID"] = 1;
+            drRow["Name"] = "test";
+            drRow["Status"] = "open";
+            drRow["CreateDate"] = DateTime.Now;
+            dr.Rows.Add(drRow);
+
+            System.Data.IDataReader idr = dr.CreateDataReader();
+
+            Conn.InTrx.Returns<bool>(false);
+            Conn.dbProvider.Returns<FFLib.Data.DBProviders.IDBProvider>(dbprovider);
+            Conn.State.Returns<System.Data.ConnectionState>(System.Data.ConnectionState.Closed);
+
+            dbprovider.ExecuteReader(null,null,null).ReturnsForAnyArgs(idr);
+
+            //Execute
+            dtoTable = new FFLib.Data.DBTable<TestDTO2>(Conn);
+            TestDTO2 result = dtoTable.Load(1);
+            Assert.AreEqual(expected.XID, result.XID);
+           
         }
 
         [Test]
         public void Save()
         {
-            MockRepository mocks = new MockRepository();
-            FFLib.Data.DBProviders.IDBProvider dbprovider = mocks.DynamicMock<FFLib.Data.DBProviders.IDBProvider>();
-            FFLib.Data.IDBConnection Conn = mocks.Stub<FFLib.Data.IDBConnection>();
+            //Initialize
+            FFLib.Data.DBProviders.IDBProvider dbprovider = Substitute.For<FFLib.Data.DBProviders.IDBProvider>();
+            FFLib.Data.IDBConnection Conn = Substitute.For<FFLib.Data.IDBConnection>();
             FFLib.Data.DBTable<TestDTO> dtoTable;
             TestDTO testData = new TestDTO();
             testData.ID = 1;
@@ -112,33 +100,26 @@ namespace FFLibUnitTests.Data
             testData.Status = "Open";
             testData.CreateDate = DateTime.Today;
 
-            using (mocks.Record())
-            {
-                dbprovider.Expect(x => x.DBUpdate(Arg<FFLib.Data.IDBConnection>.Is.Anything
-                    , Arg<string>.Is.Equal("UPDATE #__TableName SET \n[IsLocked] = @p1,[ID] = @p2,[Name] = @p3,[Status] = @p4,[CreateDate] = @p5\n WHERE [ID] = @pk")
-                    , Arg<FFLib.Data.SqlParameter[]>.Is.Anything));
-                //dbprovider.Stub(x => x.ExecuteReader(null, null, null)).IgnoreArguments().Repeat.Any().Return(idr);
-                Conn.Open();
-                LastCall.Repeat.Any();
-                Conn.Stub(x => x.InTrx).Repeat.Any().Return(false);
-                Conn.Stub(x => x.dbProvider).Return((FFLib.Data.DBProviders.IDBProvider)dbprovider).Repeat.Any();
-                Conn.Stub(x => x.State).Repeat.Any().Return(System.Data.ConnectionState.Closed);
-            }
+            //Setup
+            dbprovider.DBUpdate(NSubstitute.Arg.Any<FFLib.Data.IDBConnection>()
+                , NSubstitute.Arg.Is<string>("UPDATE #__TableName SET \n[Locked] = @p1,[ID] = @p2,[Name] = @p3,[Status] = @p4,[CreateDate] = @p5\n WHERE [ID] = @pk")
+                , NSubstitute.Arg.Any<FFLib.Data.SqlParameter[]>());
+            Conn.InTrx.Returns<bool>(false);
+            Conn.dbProvider.Returns<FFLib.Data.DBProviders.IDBProvider>(dbprovider);
+            Conn.State.Returns<System.Data.ConnectionState>(System.Data.ConnectionState.Closed);
 
-            using (mocks.Playback())
-            {
-                dtoTable = new FFLib.Data.DBTable<TestDTO>(Conn);
-                dtoTable.Save(testData);
-            }
+            //Execute
+            dtoTable = new FFLib.Data.DBTable<TestDTO>(Conn);
+            dtoTable.Save(testData);
 
         }
 
         [Test]
         public void SaveWithFieldMap()
         {
-            MockRepository mocks = new MockRepository();
-            FFLib.Data.DBProviders.IDBProvider dbprovider = mocks.DynamicMock<FFLib.Data.DBProviders.IDBProvider>();
-            FFLib.Data.IDBConnection Conn = mocks.Stub<FFLib.Data.IDBConnection>();
+            //Initialize
+            FFLib.Data.DBProviders.IDBProvider dbprovider = Substitute.For<FFLib.Data.DBProviders.IDBProvider>();
+            FFLib.Data.IDBConnection Conn = Substitute.For<FFLib.Data.IDBConnection>();
             FFLib.Data.DBTable<TestDTO2> dtoTable;
             TestDTO2 testData = new TestDTO2();
             testData.XID = 1;
@@ -146,29 +127,23 @@ namespace FFLibUnitTests.Data
             testData.Status = "Open";
             testData.CreateDate = DateTime.Today;
 
-            using (mocks.Record())
-            {
-                dbprovider.Expect(x => x.DBUpdate(Arg<FFLib.Data.IDBConnection>.Is.Anything
-                    , Arg<string>.Is.Equal("UPDATE #__TableName SET \n[Locked] = @p1,[ID] = @p2,[Name] = @p3,[Status] = @p4,[CreateDate] = @p5\n WHERE [ID] = @pk")
-                    , Arg<FFLib.Data.SqlParameter[]>.Is.Anything));
-                //dbprovider.Stub(x => x.ExecuteReader(null, null, null)).IgnoreArguments().Repeat.Any().Return(idr);
-                Conn.Open();
-                LastCall.Repeat.Any();
-                Conn.Stub(x => x.InTrx).Repeat.Any().Return(false);
-                Conn.Stub(x => x.dbProvider).Return((FFLib.Data.DBProviders.IDBProvider)dbprovider).Repeat.Any();
-                Conn.Stub(x => x.State).Repeat.Any().Return(System.Data.ConnectionState.Closed);
-            }
+            //Setup
+            dbprovider.DBUpdate(NSubstitute.Arg.Any<FFLib.Data.IDBConnection>()
+                , NSubstitute.Arg.Is<string>("UPDATE #__TableName SET \n[Locked] = @p1,[ID] = @p2,[Name] = @p3,[Status] = @p4,[CreateDate] = @p5\n WHERE [ID] = @pk")
+                , NSubstitute.Arg.Any<FFLib.Data.SqlParameter[]>());
+            Conn.InTrx.Returns<bool>(false);
+            Conn.dbProvider.Returns<FFLib.Data.DBProviders.IDBProvider>(dbprovider);
+            Conn.State.Returns<System.Data.ConnectionState>(System.Data.ConnectionState.Closed);
 
-            using (mocks.Playback())
-            {
-                dtoTable = new FFLib.Data.DBTable<TestDTO2>(Conn);
-                dtoTable.Save(testData);
-            }
+            //Execute
+            dtoTable = new FFLib.Data.DBTable<TestDTO2>(Conn);
+            dtoTable.Save(testData);
 
+
+            //Assert that all the fields exist in the SQL parameters some how.
         }
     }
-
-    class TestDTO
+class TestDTO
     {
         [FFLib.Data.Attributes.PrimaryKey]
         public int ID;
